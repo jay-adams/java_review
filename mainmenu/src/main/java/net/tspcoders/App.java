09ba349655dd76ac6634ca7f277f11454ecad4d9
Joseph Adams <jadams41@gmail.com>
2019-01-15 20:21:33 -0500

    Initial JSchool commit

---
+++ mainmenu/src/main/java/net/tspcoders/App.java
@@ -0,0 +1,13 @@
+package net.tspcoders;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
